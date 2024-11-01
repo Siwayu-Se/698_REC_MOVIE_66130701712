43664f7c3b068e64fd3c91304d43ccf2d7aa5378
Siwayu-Se <161225210+Siwayu-Se@users.noreply.github.com>
2024-11-01 19:29:39 +0700

    Update app_rec_movie_66130701712.py

--- app_rec_movie_66130701712.py
+++ app_rec_movie_66130701712.py
@@ -4,7 +4,8 @@ import pickle
 import pandas as pd
 
 # กำหนด URL หรือเส้นทางของภาพพื้นหลัง
-background_image_url = "https://images7.alphacoders.com/558/thumb-1920-558894.jpg"
+## background_image_url = "https://images7.alphacoders.com/558/thumb-1920-558894.jpg"
+background_image_url = "https://images6.alphacoders.com/133/thumbbig-1338130.webp"
 
 # กำหนดสีที่ต้องการ
 text_color = "#FFFFFF"  # สีที่คุณต้องการ
