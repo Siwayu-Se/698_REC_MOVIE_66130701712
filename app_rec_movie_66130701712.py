516fd82aa95726e45124f4c9221795c5e35c7a47
Siwayu-Se <161225210+Siwayu-Se@users.noreply.github.com>
2024-11-03 12:33:46 +0700

    Update app_rec_movie_66130701712.py

--- app_rec_movie_66130701712.py
+++ app_rec_movie_66130701712.py
@@ -6,7 +6,7 @@ import pandas as pd
 # กำหนด URL หรือเส้นทางของภาพพื้นหลัง
 background_image_url = "https://images5.alphacoders.com/373/thumb-1920-373394.png"
 # กำหนดสีที่ต้องการ
-text_color = "#CCFF33"  # สีที่คุณต้องการ
+text_color = "#99FFFF"  # สีที่คุณต้องการ
 
 # ใส่ CSS สำหรับพื้นหลังและสีตัวอักษร
 st.markdown(
